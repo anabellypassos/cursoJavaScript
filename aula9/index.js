7972ec858ce6326d0e26217d479e8be4a47995c2
anabellypassos <anabellybel123@gmail.com>
2024-10-03 15:03:14 -0300

    tipos primitivos

---
+++ aula9/index.js
@@ -0,0 +1,20 @@
+//tipos primitivos 
+
+const nome = 'Luiz'; //String
+const nome1 = " Luiz"//String
+const nome2 = `Luis`;//String
+const num1 = 10; //number
+let num2 = 10.65; //number
+let nomeAluno; //undefined -> Não aponta pra local nenhuma memória
+const sobrenome = null; //Nulo -> Não aponta pra local nenhuma memória
+const aprovado = false; // Boolean = true,façse(lógico)
+
+
+console.log(typeof nome, nome);
+//typeof  diz o valor 
+
+
+
+
+
+
