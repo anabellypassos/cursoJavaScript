b15ca3460df8f36ecdc1e3e96531dcafd0137c2e
anabellypassos <anabellybel123@gmail.com>
2024-10-04 17:09:36 -0300

    alert

---
+++ aula10/index.js
@@ -0,0 +1,78 @@
+// Operadores Aritméticos
+
+/*
+
++ Adição e concatenação 
+- Subtração
+* mutiplicação 
+/ divisão
+** potênciação
+% resto da divisão
+
+
+
+*/
+
+
+//Adição  
+ const num1 = 5;
+ const num2 = 10;
+ console.log(num1 + num2);
+
+//Concatenação 
+ const num3 = '5';
+ const num4 = 10;
+ console.log(num3 + num4);
+
+// Subtração
+const num7 =5;
+const num8 = 10;
+console.log(num7 - num8);
+
+
+
+ //* Mutiplicação 
+ const num5 = 5;
+ const num6 = 10;
+ console.log(num5 * num6);
+
+
+ //Potênciação
+ const num11 = 2;
+ const num12= 10;
+ console.log(num11 ** num12);
+
+ // Resto da divisão
+ const num13 = 2;
+ const num14= 10;
+ console.log(num13 % num14);
+
+
+
+ /*
+
+Pressedência
+()
+ **
+ *
+* /%
++ -
+ 
+ */ 
+
+
+//Icremento
+
+let contador = 1;
+contador++ ;//2
+contador++ ;//3
+contador++;//4
+
+console.log(contador)
+
+
+//Decremento 
+
+let contador2 = 10;
+contador2--;
+console.log(contador2);
---
+++ aula11/index.js
@@ -0,0 +1,22 @@
+// O usuário vai  digitar
+let num1 = prompt('Digite um numero :');
+let num2 = prompt ('Digite outro número');
+
+
+//vai exibir no console oq o usuario digitou
+console.log(num1,num2);
+
+
+//Covertendo os numeros para tipo number
+
+let num3 = prompt('Digite um numero :');
+let num4 = prompt ('Digite outro número');
+
+//coverte os numeros que estavam  como string para o tipo number
+num3 = Number(num3);
+num4 = Number(num4);
+
+
+const resultado = num3 + num4
+
+alert('O resultado da sua conta foi' + resultado);
