5ac06788c80f15dd0f94aaa1408c5ed7d994a9be
anabellypassos <anabellybel123@gmail.com>
2024-10-17 12:24:02 -0300

    function

---
+++ aula18/index.js
@@ -0,0 +1,21 @@
+
+/*
+
+function soma(x, y){
+    const resultado = x + y;
+    return resultado;
+
+
+}
+console.log(soma());
+*/
+
+
+
+
+//Função anonima
+
+const raiz = function(n){
+return n ** 0.5
+};
+console.log(raiz(9));
