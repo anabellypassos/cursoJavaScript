90171a5cc7ec960ca36372ad59ef3fcd9cd5e5fa
Anabelly Passos <anabellybel123@gmail.com>
2024-11-06 11:21:50 -0300

    array

---
+++ modulo28/atribuicaoViaDesestruturacaoArray.js
@@ -0,0 +1,7 @@
+
+const numeros = [1,2,3,4,5,6,7,8,9];
+
+const [primeiroNumero,segundoNumero,terceiroNumero,...resto]= numeros;
+
+console.log(primeiroNumero,segundoNumero,terceiroNumero);
+console.log(resto);
