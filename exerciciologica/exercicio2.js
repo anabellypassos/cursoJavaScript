edfe5dac31ca24f8627939e2daf698f462720f05
Anabelly Passos <anabellybel123@gmail.com>
2024-11-19 11:20:23 -0300

    02

---
+++ exerciciologica/exercicio2.js
@@ -0,0 +1,12 @@
+/*
+    Escreva uma função chamada ePaisagem  que 
+    recebr dois argumentos,largura e altura
+    de uma imagem (number).
+    Retorne true se a imagem estiver no modo paisagem.
+   */
+
+
+   const ePaisagem = (largura,altura) =>  largura > altura;
+       
+    
+    console.log(ePaisagem(326, 2354));
