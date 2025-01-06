4aa6fc5caff70d2fde3dd3b6e457aa8dda09dc86
Anabelly Passos <anabellybel123@gmail.com>
2025-01-06 13:58:37 -0300

    splice

---
+++ MetodoSplice/index.js
@@ -0,0 +1,8 @@
+const nomes = ['Maria','Jão','Eduardo','Gabriel','Júlia'];                  
+
+
+//nomes.splice(indice , delete ,elemento1,elemento2, elemento3);
+//pop
+
+const removidos = nomes.splice(4, 1)
+console.log(nomes,removidos);
