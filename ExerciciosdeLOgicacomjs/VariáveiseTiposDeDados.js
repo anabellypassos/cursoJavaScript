6b97944113ed48505580ff7b5ed6dd18891a19cc
Anabelly Passos <anabellybel123@gmail.com>
2025-02-17 14:33:59 -0300

    exercico de logica 1

---
+++ ExerciciosdeLOgicacomjs/VariáveiseTiposDeDados.js
@@ -0,0 +1,14 @@
+/*
+Variáveis e tipos de dados: Escreva um programa que
+declare duas variáveis, “nome” e “idade”, e as imprima 
+em um console em uma frase que diga “Olá, meu nome é [nome] e eu tenho [idade] anos”.
+
+
+*/
+
+
+let nome = 'Anabelly';
+let idade = '21';
+
+
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos!`)
