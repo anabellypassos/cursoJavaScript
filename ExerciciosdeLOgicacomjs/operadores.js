7bba3e1c08f9c8d479bb3e2d2c696d4a8985e522
Anabelly Passos <anabellybel123@gmail.com>
2025-02-21 17:37:32 -0300

    operadores execicios

---
+++ ExerciciosdeLOgicacomjs/operadores.js
@@ -0,0 +1,23 @@
+/*
+Operadores: Crie um programa que solicite ao usuário dois números e 
+exiba o resultado da soma, subtração, multiplicação e divisão desses números.
+ */
+
+
+
+var a = 3;
+var b = 4;
+
+const soma = a + b;
+const subtração = a - b;
+const mutiplicação = a * b;
+const divisao = a / b;
+
+console.log(
+    `O valor dos números são : ${a} e ${b}.
+      O resultado da soma deles é : ${soma}.
+      O resultado da subtração deles é: ${subtração}.
+      O resultado da mulltiplicação  desses dois números é : ${mutiplicação}.
+      O resultado da divisão é : ${divisao}
+     `
+);
