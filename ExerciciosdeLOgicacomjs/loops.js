6e8dbfacf38b638f715ab7727bdb17e71a5437d8
Anabelly Passos <anabellybel123@gmail.com>
2025-03-10 10:45:09 -0300

    Exercicio de logica com loop

---
+++ ExerciciosdeLOgicacomjs/loops.js
@@ -0,0 +1,23 @@
+/*
+Loops: Crie um loop que imprima todos os números de 1 a 100.
+*/
+
+
+
+for (let i = 1; i <=100; i++){
+console.log(i);
+}
+
+
+
+/*
+Define i = 1 antes do loop.
+Executa enquanto i <= 100.
+Incrementa i++ no final de cada iteração.
+
+*/ 
+let i = 1;
+do{
+    console.log(i);
+    i++;
+}while(i <=100);
