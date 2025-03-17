e6ac8ccce787ca03b1ff1bc21f2a0ed601cb0930
Anabelly Passos <anabellybel123@gmail.com>
2025-03-17 19:03:05 -0300

    arquivos estaticos

---
+++ node/aula11/src/controllers/homeController.js
@@ -0,0 +1,10 @@
+exports.paginaInicial = (req, res) => {
+    res.render('index');
+
+        
+        }
+
+exports.trataPost = (req,res)=>{
+            res.send('Ei sou a sua nova rota de POST');
+
+}
